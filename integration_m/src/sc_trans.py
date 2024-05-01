9044d5d2c330ea7317744024cddac821b705d85a
shapsider <54304747+shapsider@users.noreply.github.com>
2024-05-01 18:06:41 +0800

    Delete integration_m/src directory

--- integration_m/src/sc_trans.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-import torch
-
-encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
-x = torch.rand(10, 2000, 512)
-for _ in range(2):
-    x = encoder_layer(x)
-
-print(x.size())
